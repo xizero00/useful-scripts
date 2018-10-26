24b1fe1ce385d8e0398609f701e6572c1340bc57
djangogo <xizero00@gmail.com>
2018-10-27 02:21:13 +0800

    add more

--- python_install.sh
+++ python_install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-sudo -H pip install jupyter
+sudo -H pip install tqdm
+sudo -H pip install paramiko
+
+sudo -H pip3 install matplotlib
+sudo -H pip3 install tqdm
+sudo -H pip3 install paramiko
+
+
