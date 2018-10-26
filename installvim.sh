f29d53d8de9e07bc168878d6744dee5fb4d4ba28
djangogo <xizero00@gmail.com>
2018-10-27 01:05:02 +0800

    add my config

--- installvim.sh
+++ installvim.sh
@@ -2,4 +2,9 @@
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 sudo apt install -y vim
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+proxychains git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# vim ~/.vim_runtime/my_configs.vim
+# set mouse=a
+# set nu
