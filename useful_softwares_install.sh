37c0c645fc611ad5f864d12941dded093b89094b
djangogo <xizero00@gmail.com>
2018-10-27 01:17:58 +0800

    useful software install

---
+++ useful_softwares_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt install -y \
+    htop \
+    git \
+    dstat \
+    sshfs \
+    mc \
+    cmake \
+    psensor\
+    lm-sensors \
+    unrar \
+    uget \
+    p7zip-full \
+    ssh \
+    silversearcher-ag \
+    ntop \
+
