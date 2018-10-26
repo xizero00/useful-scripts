7f43765a3c893f34585ce75bb5a329ef940be8ea
djangogo <xizero00@gmail.com>
2018-10-27 02:28:00 +0800

    rm microsoft.gpg

--- useful_software_install.sh
+++ useful_software_install.sh
@@ -22,7 +22,7 @@ vscode_install(){
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
+    rm microsoft.gpg
     sudo apt-get install -y apt-transport-https
     sudo apt-get update
     sudo apt-get install -y code # or code-insiders
