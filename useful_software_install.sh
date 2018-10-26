24b8753a847d38cbef1685e95a9a99fa7b855965
djangogo <xizero00@gmail.com>
2018-10-27 02:26:56 +0800

    add vscode

--- useful_software_install.sh
+++ useful_software_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+bundle_install(){
 sudo apt install -y \
     htop \
     git \
@@ -14,4 +15,18 @@ sudo apt install -y \
     p7zip-full \
     ssh \
     silversearcher-ag
+}
 
+# vscode
+vscode_install(){
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y code # or code-insiders
+}
+
+#bundle_install
+vscode_install
