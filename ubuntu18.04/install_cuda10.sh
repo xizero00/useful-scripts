e94f28d35e66860c631ac46e03836af4ce41879d
djangogo <xizero00@gmail.com>
2019-04-08 17:26:03 +0800

    comment the key receiving

--- ubuntu18.04/install_cuda10.sh
+++ ubuntu18.04/install_cuda10.sh
@@ -7,12 +7,14 @@ install_repos(){
 
     # CUDA
     # From https://askubuntu.com/questions/1077061/how-do-i-install-nvidia-and-cuda-drivers-into-ubuntu/1077063#1077063
-    sudo ${PROXYCHAINS} apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+    #sudo ${PROXYCHAINS} apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
     sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
-
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv F60F4B3D7FA2AF80
+    
     # CUDNN and NCCL
     ${PROXYCHAINS} wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
     sudo dpkg -i nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+    #sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/7fa2af80.pub
 
     # Tensor RT
     ${PROXYCHAINS} wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvinfer-runtime-trt-repo-ubuntu1804-5.0.2-ga-cuda10.0_1-1_amd64.deb
